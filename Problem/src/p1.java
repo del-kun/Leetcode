dcf935718995fc42c655c02a60c3c6ed7c6e2590
del-kun <358789467@qq.com>
2019-12-04 14:38:45 +0800

    -

---
+++ Problem/src/p1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] test = new int[2];
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[i]+nums[j] == target)
+                {
+                    test[0] = i;
+                    test[1] = j;
+                    break;
+                }
+            }
+        }
+        return test;
+    }
+}
